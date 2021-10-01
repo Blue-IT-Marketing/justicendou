e37289b577db5b10828d99a3a907dd83526bd290
mobius-crypt <mobiusndou@gmail.com>
2021-10-01 11:02:51 +0200

    fetch refactored

--- src/fetch_utils.py
+++ src/fetch_utils.py
@@ -1,15 +1,17 @@
-from typing import Optional
 import aiohttp
 
 
-async def async_post_request(_url, json_data, headers) -> tuple:
+async def async_post_request(_url: str, json_data: dict, headers: dict) -> tuple:
     async with aiohttp.ClientSession() as session:
-        async with session.post(url=_url, json=json_data, headers=headers) as response:
-            json_data: dict = await response.json()
-            return json_data, response.status
+        try:
+            async with session.post(url=_url, json=json_data, headers=headers) as response:
+                json_data: dict = await response.json()
+                return json_data, response.status
+        except aiohttp.ClientConnectorError:
+            return None, 406
 
 
-async def async_get_request(_url, headers) -> tuple:
+async def async_get_request(_url: str, headers: dict) -> tuple:
     async with aiohttp.ClientSession() as session:
         try:
             async with session.get(url=_url, headers=headers) as response:
