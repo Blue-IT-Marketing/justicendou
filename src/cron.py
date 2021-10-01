92183d316177020e3f8a982dac05c04352857a56
mobius-crypt <mobiusndou@gmail.com>
2021-10-01 11:00:32 +0200

    updated cron jobs to run on a thread

--- src/cron.py
+++ src/cron.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask import current_app
+from threading import Thread
 from src.articles import Articles
 
 cron_route_bp = Blueprint('cron_route', __name__)
@@ -6,5 +8,14 @@ cron_route_bp = Blueprint('cron_route', __name__)
 
 @cron_route_bp.route('/_cron/articles', methods=['GET', 'POST'])
 def cron_articles():
-    Articles().cron_daily_topics()
+    """
+        **cron_articles**
+            run cron articles in a separate thread
+    :return:
+    """
+    current_app.articles_cron_jobs = Thread(target=Articles().cron_daily_topics)
+    try:
+        current_app.articles_cron_jobs.run()
+    except RuntimeError:
+        return 'Error', 500
     return 'OK', 200
