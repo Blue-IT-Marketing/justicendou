fea1f542cbb6bba8b2b2178f848f252f66f17fb0
mobius-crypt <mobiusndou@gmail.com>
2021-10-25 11:22:27 +0200

    code refactored

--- src/navigation.py
+++ src/navigation.py
@@ -2,23 +2,24 @@ import os
 import jinja2
 from flask import Blueprint, request, render_template
 template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getcwd()))
-
+# 0763088022
 navigation_loader = Blueprint('navigation_loader', __name__)
 
 
 @navigation_loader.route('/navigation/<string:path>', methods=['GET', 'POST'])
 def navigation(path: str):
+    """navigation routes"""
     if path == "header":
         template = template_env.get_template('dynamic/navigation/header.html')
-        context = {}
+        context = dict()
         return template.render(context), 200
     elif path == "sidebar":
         template = template_env.get_template('dynamic/navigation/sidebar.html')
-        context = {}
+        context = dict()
         return template.render(context), 200
     elif path == "footer":
         template = template_env.get_template('dynamic/navigation/footer.html')
-        context = {}
+        context = dict()
         return template.render(context), 200
     else:
         pass
