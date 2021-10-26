c2dc5fa51e1d5d7dbbc37b92f3f8b1a725e05846
mobius-crypt <mobiusndou@gmail.com>
2021-10-26 15:44:31 +0200

    refactored template lookup on blog handler

--- src/blog.py
+++ src/blog.py
@@ -1,10 +1,7 @@
-import logging
 import os
-import jinja2
-from flask import Blueprint, render_template, request
-import datetime
 
-from jinja2 import Template
+import jinja2
+from flask import Blueprint, render_template
 
 template_env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.getcwd()))
 
@@ -17,20 +14,20 @@ def get_url(path: str) -> str:
 
 # TODO- consider methods of obtaining and storing old articles and then submitting them as secong pages indexed by dates
 
-template_lookup: dict = dict(
-    programming=template_env.get_template('justice-ndou/blog/categories/programming/programming.html'),
-    science=template_env.get_template('justice-ndou/blog/categories/science/science.html'),
-    philosophy=template_env.get_template('justice-ndou/blog/categories/philosophy/philosophy.html'),
-    mathematics=template_env.get_template('justice-ndou/blog/categories/mathematics/mathematics.html'),
-    hacking=template_env.get_template('justice-ndou/blog/categories/hacking/hacking.html'),
-    networking=template_env.get_template('justice-ndou/blog/categories/networking/networking.html'),
-    ai=template_env.get_template('justice-ndou/blog/categories/ai/ai.html')
-)
-
 
 @blog_handler_bp.route('/blog/<string:path>', methods=['GET'])
 def blog_handler(path: str):
     # Will throw an error if route/ template not found
+    template_lookup: dict = dict(
+        programming=template_env.get_template('justice-ndou/blog/categories/programming/programming.html'),
+        science=template_env.get_template('justice-ndou/blog/categories/science/science.html'),
+        philosophy=template_env.get_template('justice-ndou/blog/categories/philosophy/philosophy.html'),
+        mathematics=template_env.get_template('justice-ndou/blog/categories/mathematics/mathematics.html'),
+        hacking=template_env.get_template('justice-ndou/blog/categories/hacking/hacking.html'),
+        networking=template_env.get_template('justice-ndou/blog/categories/networking/networking.html'),
+        ai=template_env.get_template('justice-ndou/blog/categories/ai/ai.html')
+    )
+
     request_url_list = get_url(path).split("/")
     return render_template(template_lookup[request_url_list[-1]])
 
