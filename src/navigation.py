2b72d93a2d04fea3f95d565dc74594cd86a26b36
mobius-crypt <mobiusndou@gmail.com>
2021-10-26 17:34:57 +0200

    code refactored

--- src/navigation.py
+++ src/navigation.py
@@ -9,17 +9,6 @@ navigation_loader = Blueprint('navigation_loader', __name__)
 @navigation_loader.route('/navigation/<string:path>', methods=['GET', 'POST'])
 def navigation(path: str):
     """navigation routes"""
-    if path == "header":
-        template = template_env.get_template('dynamic/navigation/header.html')
-        context = dict()
-        return template.render(context), 200
-    elif path == "sidebar":
-        template = template_env.get_template('dynamic/navigation/sidebar.html')
-        context = dict()
-        return template.render(context), 200
-    elif path == "footer":
-        template = template_env.get_template('dynamic/navigation/footer.html')
-        context = dict()
-        return template.render(context), 200
-    else:
-        pass
+    return dict(header=template_env.get_template('dynamic/navigation/header.html'),
+                sidebar=template_env.get_template('dynamic/navigation/sidebar.html'),
+                footer=template_env.get_template('dynamic/navigation/footer.html')).get(path).render(dict()), 200
