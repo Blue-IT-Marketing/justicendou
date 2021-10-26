ab2f49c11ffe440904898695cc57a9a7b11b9869
mobius-crypt <mobiusndou@gmail.com>
2021-10-26 17:48:46 +0200

    code refactored

--- src/profiles.py
+++ src/profiles.py
@@ -9,18 +9,9 @@ profiles_bp = Blueprint('profiles', __name__)
 @profiles_bp.route('/profiles/<string:path>', methods=['GET'])
 def profiles(path: str):
     """profile routes"""
-    if path == "software-projects":
-        template = template_env.get_template(
-            'justice-ndou/personal-profile/software-projects/software-projects.html')
-        context = dict()
-    elif path == "linkedin":
-        template = template_env.get_template('justice-ndou/personal-profile/linkedin-profile/linkedin.html')
-        context = dict()
-
-    elif path == "services":
-        template = template_env.get_template('justice-ndou/personal-profile/services/services.html')
-        context = dict()
-    else:
-        return
-
-    return template.render(context), 200
+    software_projects_template = 'justice-ndou/personal-profile/software-projects/software-projects.html'
+    linked_in_template = 'justice-ndou/personal-profile/linkedin-profile/linkedin.html'
+    services_template = 'justice-ndou/personal-profile/services/services.html'
+    return dict(software_projects=template_env.get_template(software_projects_template),
+                linkedin=template_env.get_template(linked_in_template),
+                services=template_env.get_template(services_template)).get(path.replace('-', '_')).render(dict()), 200
