c32e0e0ac204a673bcb44e23c980bff3220f16f5
mobius-crypt <mobiusndou@gmail.com>
2021-10-29 14:05:24 +0200

    code refactored project status

--- src/navigation.py
+++ src/navigation.py
@@ -8,7 +8,8 @@ navigation_loader = Blueprint('navigation_loader', __name__)
 
 @navigation_loader.route('/navigation/<string:path>', methods=['GET', 'POST'])
 def navigation(path: str):
-    """navigation routes"""
+    """**navigation**
+    routes"""
     return dict(header=template_env.get_template('dynamic/navigation/header.html'),
                 sidebar=template_env.get_template('dynamic/navigation/sidebar.html'),
                 footer=template_env.get_template('dynamic/navigation/footer.html')).get(path).render(dict()), 200
