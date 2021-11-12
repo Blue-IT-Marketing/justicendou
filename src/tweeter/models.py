e94bfe8d52b5014662ccb013d924c1db70c56e06
mobius-crypt <mobiusndou@gmail.com>
2021-11-12 22:40:26 +0200

    refacrored

--- src/tweeter/models.py
+++ src/tweeter/models.py
@@ -20,7 +20,28 @@ class TweetModel(ndb.Model):
     user_listed_count: int = ndb.IntegerProperty()
 
 
-    
+    def __str__(self) -> str:
+        """string represantation of tweet"""
+        return f"id: {self.tweet_id} tweeted by: {self.user_screen_name} tweet: {self.text}"
+
+
+    def consume_tweet(self, tweet) -> ndb.Key:
+        """
+            **Consumes a tweet and stores it in the datastore**
+        """
+        self.tweet_id = tweet.id
+        self.id_str = tweet.id_str
+        self.text = tweet.text
+        self.created_at = tweet.created_at
+        self.user_id = tweet.user.id
+        self.user_name = tweet.user.name
+        self.user_screen_name = tweet.user.screen_name
+        self.user_location = tweet.user.location
+        self.user_description = tweet.user.description
+        self.user_followers_count = tweet.user.followers_count
+        self.user_friends_count = tweet.user.friends_count
+        self.user_listed_count = tweet.user.listed_count
+        return self.put()
 
 
 
