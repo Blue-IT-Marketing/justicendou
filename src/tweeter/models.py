f284c1745b329ace047103988c973569296e3a67
mobius-crypt <mobiusndou@gmail.com>
2021-11-12 21:59:13 +0200

    code refactored

--- src/tweeter/models.py
+++ src/tweeter/models.py
@@ -1,11 +1,31 @@
 
+from google.cloud import ndb
 
 
-class TweetModel:
+class TweetModel(ndb.Model):
     """
         **A Google Datastore Model to store Tweets**
     """
+    tweet_id: str = ndb.StringProperty()
+    tweet_text: str = ndb.StringProperty()
+    tweet_created_at: str = ndb.StringProperty()
+    tweet_user_id: str = ndb.StringProperty()
+    tweet_user_name: str = ndb.StringProperty()
+    tweet_user_screen_name: str = ndb.StringProperty()
+    tweet_user_location: str = ndb.StringProperty()
+    tweet_user_description: str = ndb.StringProperty()
+    tweet_user_followers_count: int = ndb.IntegerProperty()
+    tweet_user_friends_count: int = ndb.IntegerProperty()
+    tweet_user_listed_count: int = ndb.IntegerProperty()
+
+
     
+
+
+
+
+
+
 
 
     
