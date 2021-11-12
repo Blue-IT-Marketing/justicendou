d3b4daceed8213610fff6676be077c69dd5b67f2
mobius-crypt <mobiusndou@gmail.com>
2021-11-12 22:04:22 +0200

    TweetModel Refactored

--- src/tweeter/models.py
+++ src/tweeter/models.py
@@ -7,16 +7,17 @@ class TweetModel(ndb.Model):
         **A Google Datastore Model to store Tweets**
     """
     tweet_id: str = ndb.StringProperty()
-    tweet_text: str = ndb.StringProperty()
-    tweet_created_at: str = ndb.StringProperty()
-    tweet_user_id: str = ndb.StringProperty()
-    tweet_user_name: str = ndb.StringProperty()
-    tweet_user_screen_name: str = ndb.StringProperty()
-    tweet_user_location: str = ndb.StringProperty()
-    tweet_user_description: str = ndb.StringProperty()
-    tweet_user_followers_count: int = ndb.IntegerProperty()
-    tweet_user_friends_count: int = ndb.IntegerProperty()
-    tweet_user_listed_count: int = ndb.IntegerProperty()
+    id_str: str = ndb.StringProperty()
+    text: str = ndb.StringProperty()
+    created_at: str = ndb.StringProperty()
+    user_id: str = ndb.StringProperty()
+    user_name: str = ndb.StringProperty()
+    user_screen_name: str = ndb.StringProperty()
+    user_location: str = ndb.StringProperty()
+    user_description: str = ndb.StringProperty()
+    user_followers_count: int = ndb.IntegerProperty()
+    user_friends_count: int = ndb.IntegerProperty()
+    user_listed_count: int = ndb.IntegerProperty()
 
 
     
